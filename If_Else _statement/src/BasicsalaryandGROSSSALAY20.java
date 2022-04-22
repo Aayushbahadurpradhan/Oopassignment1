ee75fc0348b317215ad0db29f3f5f4e6ae6a3bf6
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2022-04-22 13:37:12 +0545

    adding q 20 and 21

--- If_Else _statement/src/BasicsalaryandGROSSSALAY20.java
+++ If_Else _statement/src/BasicsalaryandGROSSSALAY20.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 /*20. Write a JAVA program to input basic salary of an employee and calculate its Gross salary
 according to following:
 Basic Salary <= 10000 : HRA = 20%, DA = 80%
 */
 public class BasicsalaryandGROSSSALAY20 {
     public static void main(String[] args) {
-
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter your basic salary: ");
+        double basic_salary= scanner.nextDouble();
+        if ( basic_salary<=10000){
+            double grossSalary = basic_salary +0.2*basic_salary+0.8*basic_salary;
+            System.out.println(grossSalary);
+        }else if (basic_salary<= 20000){
+            double grossSalary = basic_salary +0.25*basic_salary+0.9*basic_salary;
+            System.out.println(grossSalary);
+        }else if (basic_salary>20000){
+            double grossSalary = basic_salary +0.3*basic_salary+0.95*basic_salary;
+            System.out.println(grossSalary);
+        }else {
+            System.out.println("Invalid data");
+        }
     }
 }
--- If_Else _statement/src/ElectricityBill21.java
+++ If_Else _statement/src/ElectricityBill21.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
+/*21. Write a JAVA program to input electricity unit charges and calculate total electricity bill
+according to the given condition:
+For first 50 units Rs. 0.50/unit
+For next 100 units Rs. 0.75/unit
+For next 100 units Rs. 1.20/unit
+For unit above 250 Rs. 1.50/unit
+An additional surcharge of 20% is added to the bill*/
 public class ElectricityBill21 {
     public static void main(String[] args) {
-
+        int Units;
+        double Amount, Sur_charge, Total_Amount;
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter the units u have consumed: ");
+        Units=scanner.nextInt();
+        if (Units<=50){
+            Amount= Units*0.5;
+            Sur_charge= 0.2*Amount;
+        }else if (Units<=150){
+            Amount=25+((Units-50)*0.75);
+            Sur_charge =0.2*Amount;
+        }else if (Units<=250){
+            Amount=25+75+((Units-150)*1.20);
+            Sur_charge =0.2*Amount;
+        }else {
+            Amount=25+75+120+((Units-250)*1.50);
+            Sur_charge=0.2*Amount;
+        }
+        Total_Amount = Amount+Sur_charge;
+        System.out.println("\n Electricity bill ="+Total_Amount);
     }
 }
